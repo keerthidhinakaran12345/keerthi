015fc11d87b89cc99693c9f48bb03bb39a44592b
keerthidhinakaran12345 <43365616+keerthidhinakaran12345@users.noreply.github.com>
2019-03-05 12:05:55 +0530

    fibonacci series.c

---
+++ fibonacci series .c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+void printFibonacciNumbers(int n) 
+{ 
+    int f1=0, f2=1,i; 
+    if(n<1) 
+        return; 
+    for (i=1;i<=n;i++) 
+    { 
+        printf("%d",f2); 
+        int next=f1+f2; 
+        f1=f2; 
+        f2=next; 
+    }
+} 
+  
+  int main() 
+{ 
+    printFibonacciNumbers(7); 
+    return 0; 
+} 
